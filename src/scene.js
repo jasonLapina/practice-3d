9b4cc8fa879501bef9f201fc8cccc4849e18ed75
Jason Lapina <jasonlapina.blink@gmail.com>
2024-01-23 11:03:29 +0800

    randiverse

--- src/scene.js
+++ src/scene.js
@@ -23,6 +23,10 @@ const Scene = () => {
     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/skunk/model.gltf"
   );
 
+  const { scene: jfc } = useGLTF(
+    "https://d1bh4bdsrj0ehq.cloudfront.net/Worlds/world1.glb"
+  );
+
   const randimations = useAnimations(animations, randy);
 
   useEffect(() => {
@@ -43,24 +47,29 @@ const Scene = () => {
       <Sparkles speed={0.5} color='orange' position-y={0.3} />
       <Sparkles speed={0.5} color='purple' position-y={0.3} />
       <Sky sunPosition={[0, -5, -200]} />
-      <Cloud scale={[0.2, 0.2, 0.2]} position-y={-0.3} />
+      <Cloud color='red' scale={[0.2, 0.2, 0.2]} position-y={-0.3} />
       <Stars />
-      <Text3D font='/Super Spicy_Regular.json' position={[-2.1, 2, 0]}>
-        Randy
+      <Text3D font='/Super Spicy_Regular.json' position={[-3.5, 2, 0]}>
+        Randiverse
         <meshNormalMaterial />
       </Text3D>
       <primitive castShadow object={randy} />
 
-      <Text3D font='/Super Spicy_Regular.json' position={[3.5, 2, 0]}>
+      {/* <Text3D font='/Super Spicy_Regular.json' position={[3.5, 2, 0]}>
         Utes
         <meshNormalMaterial />
-      </Text3D>
+      </Text3D> */}
       <primitive
         castShadow
         object={pig}
         position={[5, 0, 0]}
         scale={[0.5, 0.5, 0.5]}
         ref={pigref}
+      />
+      <primitive
+        object={jfc}
+        scale={[0.3, 0.3, 0.3]}
+        position={[0, -1, -0.9]}
       />
     </>
   );
